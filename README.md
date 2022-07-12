cf20bc3790ff7e168272cddb0d23cfb003f88586
gonzalote99 <gonzalodavila759@gmail.com>
2022-07-12 17:07:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-yzvete
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-yzvete)
---
+++ script.js
@@ -0,0 +1,25 @@
+function generateColor() {
+  let text = document.getElementById("text");
+  let x = Math.floor((Math.random() * 256 ) + 1 );
+  let y = Math.floor((Math.random() * 256 ) + 1 );
+  let z = Math.floor((Math.random() * 256 ) + 1);
+  let r = toHex(x);
+  let g = toHex(y);
+  let b = toHex(z);
+  document.body.style.backgroundColor = "rgb(" + x + ", " + y + ", " + z + ")";
+  text.innerHTML = "background generated: rgb(" + x + ", " + y + ", " + z + ")<br/>Hex: #" + r + g + b;
+    
+}
+
+generateColor();
+let button = document.getElementById("button");
+button.addEventListener("click", function()  {
+generateColor();
+
+});
+
+function toHex(n) {
+ var hex = n.toString(16);
+ while (hex.length < 2 ) { hex = 0, + hex}
+ return hex;
+}
